80e8f77f283960b79b837004b28fe0e8855f9b93
GBTC440010UR <GBTC440010UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-26 15:19:49 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+    public class HomeController{
+    @RequestMapping("/loadForm")
+    public String loadFormpages() {
+        return "formtemplate";
+    }
+
+        @RequestMapping("/processForm")
+        public String loadFormPage(@RequestParam("login") String login , Model model)  {
+            System.out.println(login);
+            model.addAttribute("loginval", login);
+            return "confirm";
+    }
+}
